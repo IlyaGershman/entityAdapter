f78885057fe7c1aa58bb5b90213a2a7a19a49957
IlyaGershman <IlyaGershman@users.noreply.github.com>
2021-10-10 09:57:08 +0300

    Initial commit

---
+++ src/services/normalize/load.js
@@ -0,0 +1,24 @@
+const data = [
+  {
+    id: "workflow1",
+    type: "workflow",
+    name: "wf1",
+    steps: [
+      { id: "step1", type: "step", name: "step 1" },
+      { id: "step2", type: "step", name: "step 2" }
+    ]
+  },
+  { id: "step1", type: "step", name: "step 1" },
+  { id: "step2", type: "step", name: "step 2" },
+  { id: "step3", type: "step", name: "step 3" },
+  { id: "step4", type: "step", name: "step 4" },
+  {
+    id: "workflow2",
+    type: "workflow",
+    name: "wf2",
+    steps: [
+      { id: "step3", type: "step", name: "step 3" },
+      { id: "step4", type: "step", name: "step 4" }
+    ]
+  }
+];
